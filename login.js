474391102a99211267c0a28225d3f46085931e83
meenakshiguna <46318340+meenakshiguna@users.noreply.github.com>
2021-10-06 20:50:14 +0530

    Initial commit

---
+++ login.js
@@ -0,0 +1,64 @@
+const txtEmail = document.getElementById("signEmail")
+const txtPassword = document.getElementById("signPassword")
+const btnLogin = document.getElementById("Loginbtn");
+var uid;
+document.getElementById("Loginbtn").addEventListener('click', e => {
+//Login verification code
+    e.preventDefault();
+    const email = txtEmail.value;
+    const password = txtPassword.value;
+    const auth = firebase.auth();
+    localStorage.setItem('email', email)
+    localStorage.setItem('password', password)
+    const promise = auth.signInWithEmailAndPassword(email, password);
+    promise.then(e => {
+        uid = e.user.uid;
+        localStorage.setItem('uid', e.user.uid)
+
+        db.collection('users').doc(uid).get().then(snapshot => {
+            alert("Hello " + snapshot.data().Name + "!\r\nWelcome!")
+            localStorage.setItem("location", snapshot.data().Location)
+            location.href = "index.html";
+        })
+
+    })
+    promise.catch(e => {
+        console.log(e.message)
+        alert(e.message);
+    })
+}
+)
+
+document.getElementById("forgotPassword").addEventListener('click', e => {
+//function forgotPassword(){
+      e.preventDefault();
+      var auth = firebase.auth();
+      const email = document.getElementById('signEmail').value
+      if(email !== "")
+      {
+          auth.sendPasswordResetEmail(email).then(function(){
+                window.alert("Email has been sent to you. Please check your inbox.")
+          })
+          .catch(function(error){
+              alert(error.message)
+              console.log(error.message)
+          })
+      }
+  }
+)
+
+function show() {
+    //Code for viewing typed pasword
+    var icon = document.getElementById("icon")
+    if (txtPassword.type === "password") {
+        txtPassword.type = "text"
+        icon.style.color = "grey"
+    } else {
+        txtPassword.type = "password"
+        icon.style.color = '#eeeeee';
+    }
+}
+
+
+
+
---
+++ signUp.js
@@ -0,0 +1,44 @@
+const txtEmail = document.getElementById("Email")
+const txtPassword = document.getElementById("password")
+const txtName = document.getElementById("Name")
+
+var txtGrade = document.getElementById("grade");
+document.getElementById("Register").addEventListener('click', e => {
+    e.preventDefault();
+    const email = txtEmail.value;
+    const password = txtPassword.value;
+    const name = txtName.value
+   
+   if (email == "" || password == "" || name == "") {
+                alert("Please enter all the fields");
+            } else {
+                const promise = firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .then(cred => {
+                        //Store the information in the database
+                        db.collection('users').doc(cred.user.uid).set({
+                            Email: email,
+                            Name: name,
+                            Password: password
+                        }).then(() => {
+                            if(confirm("You have been successfully registered.")){
+                            window.location.href = "Login.html"}
+                        });
+
+                    })
+                promise.catch(e => {
+                    console.log(e.message)
+                    alert(e.message);
+                })
+            }
+})
+function show() {
+    //Code to view the typed password
+    var icon = document.getElementById("icon")
+    if (txtPassword.type === "password") {
+        txtPassword.type = "text"
+        icon.style.color = "grey"
+    } else {
+        txtPassword.type = "password"
+        icon.style.color = '#eeeeee';
+    }
+}
